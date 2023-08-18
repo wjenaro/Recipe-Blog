1769e2669937580d4473bc08eb49ae8a8e826c0a
Jenaro Wachira <wjenaro@gmail.com>
2023-08-19 02:22:17 +0300

    ---

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,9 +1,10 @@
+require('../models/database');
+
 /**
- * GET/
- * homepage
- * Renders the index view for the homepage
- * @param {Object} req - The request object
- * @param {Object} res - The response object
+ * Renders the homepage.
+ * 
+ * @param {object} req - The request object.
+ * @param {object} res - The response object.
  */
 exports.homepage = async (req, res) => {
   try {
--- server/models/database.js
+++ server/models/database.js
@@ -1,6 +1,19 @@
-// const mongoose = require('mongoose');
-//const url='mongodb+srv://coder:SAZdh3ro0GLuRvfq@cluster0.9qaxudy.mongodb.net/?retryWrites=true&w=majority';
+const mongoose = require('mongoose');
+
 // mongoose.connect('mongodb://0.0.0.0:27017/recipeDB', { useNewUrlParser: true, useUnifiedTopology: true });
+async function connectDB(){
+    try {
+        mongoose.connect("mongodb+srv://coder:SAZdh3ro0GLuRvfq@cluster0.9qaxudy.mongodb.net/?retryWrites=true&w=majority");
+        console.log("Database connected ---")
+    } catch (error) {
+        console.log("failed to connect");
+        
+    }
+
+}
+
+
+
 
 // const db = mongoose.connection;
 // db.on('error', console.error.bind(console, 'connection error:'));
@@ -9,4 +22,4 @@
 // });
 
 // // Models
-// require('./Category');
+//require('./Category');
